b3fe5a2b3f27a4f1574d701a7e47de4b8643b2c3
Alisa Filatova <alicefill@bk.ru>
2017-05-10 20:47:56 +0300

    исправила меню

--- js/main-nav.js
+++ js/main-nav.js
@@ -5,11 +5,11 @@ var close = document.querySelector('.close-btn');
 open.addEventListener('click', function(event) {
      event.preventDefault();
      nav.classList.add('main-nav_open');
-     open.classList.remove('menu-open');
+     open.classList.add('menu-open_hidden');
 });
 
 close.addEventListener('click', function(event) {
     event.preventDefault();
     nav.classList.remove('main-nav_open');
-    open.classList.add('menu-open');
+    open.classList.remove('menu-open_hidden');
 });
